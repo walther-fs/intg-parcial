341a141b119202a00875c9c1960d322532573226
WALTHER <walther.fs4@gmail.com>
2022-05-28 18:27:03 -0500

    -add module

---
+++ spring-boot/src/main/java/parcial/isil/Application.java
@@ -0,0 +1,13 @@
+package parcial.isil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
